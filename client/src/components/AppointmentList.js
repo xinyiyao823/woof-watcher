d3970a655f558f05b865e6dc305918c7f72dc019
+ <+>
2021-12-01 23:46:33 -0600

    some css

--- client/src/components/AppointmentList.js
+++ client/src/components/AppointmentList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Appointment from './Appointment'
 import styled from 'styled-components'
+import { BiArrowBack } from 'react-icons/bi'
+import { useNavigate } from 'react-router-dom'
 
 
 function AppointmentList({user, appointment, setAppointments}) {
 
+
+    const navigate = useNavigate()
     const handleDeleteAppt = (selectedAppt) => {
         fetch(`/appointments/${selectedAppt.id}`, { 
           method: 'DELETE'
@@ -18,6 +22,7 @@ function AppointmentList({user, appointment, setAppointments}) {
     console.log(user.appointments)
     return (
         <div>
+            <Button onClick={() => navigate('/home')}><BiArrowBack /></Button>
             <Header>{user.name}'s Appointments</Header>
             {user.appointments.map(appointment => <Appointment appointment={appointment} handleDeleteAppt={handleDeleteAppt} />)}
         </div>
@@ -28,4 +33,14 @@ export default AppointmentList
 
 const Header = styled.h1`
   font-family: 'Fuzzy Bubbles', cursive;
+`
+const Button = styled.button`
+    margin: 15px;
+    border-radius: 30px;
+    border: none;
+    font-color: white;
+    padding: 10px;
+    font-family: 'Fuzzy Bubbles', cursive;
+    cursor: pointer;
+    font-size: 15px;
 `
